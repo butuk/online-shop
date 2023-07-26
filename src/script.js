75ce480bc418328e0736241f0453c7d71aee8d71
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-07-26 10:46:03 +0200

    feat: add click on product and filter by popular

--- src/script.js
+++ src/script.js
@@ -3,7 +3,13 @@ import {products} from "./scripts/products.js";
 Vue.createApp ({
   data() {
     return {
+      popularOnly: false,
       items: products.items
     }
-  }
+  },
+  computed: {
+    showItems() {
+      return this.items.filter(item => this.popularOnly ? item.popular : item)
+    }
+  },
 }).mount('#app');
