ac4bb6a72dcedc5cf6bbd6cd8c64a9b5708c4d40
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-07-27 21:57:07 +0200

    feat: make size, type, color and creator filters

--- src/script.js
+++ src/script.js
@@ -4,12 +4,24 @@ Vue.createApp ({
   data() {
     return {
       popularOnly: false,
-      items: products.items
+      size: [],
+      type: [],
+      color: [],
+      creator: [],
+      items: products.items,
     }
   },
   computed: {
     showItems() {
-      return this.items.filter(item => this.popularOnly ? item.popular : item)
+      return this.items
+        .filter(item => name ? item.name.toLowerCase() === this.name.toLowerCase() : item)
+        .filter(item => this.popularOnly ? item.popular : item)
+        .filter(item => this.size.length >= 1 ? this.size.includes(item.size) : item)
+        .filter(item => this.type.length >= 1 ? this.type.includes(item.type) : item)
+        .filter(item => this.color.length >= 1 ? this.color.includes(item.color) : item)
+        .filter(item => this.creator.length >= 1 ? this.creator.includes(item.creator.toLowerCase()) : item)
     }
   },
 }).mount('#app');
+
+
